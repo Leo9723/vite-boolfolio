40ad23bf27df37d00db2eaca903afcac8220fba1
Leo9723 <pala9761@gmail.com>
2023-03-10 22:44:26 +0100

    aggiunta pagina di errore in caso di fallita chiamata e relativi caricamenti

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    
+}
+</script>
+
+
+<template>
+    <div>
+        <h1>ERRORE 404! La tua pagina non è stata trovata</h1>
+        <p>Torna alla lista dei progetti da 
+            <router-link :to="{ name: 'project_list' }" class="btn btn-sm btn-success">
+                                qui
+             </router-link>
+         </p>
+    </div>
+</template>
+
+
+<style lang="">
+    
+</style>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -15,8 +15,15 @@ export default {
     mounted(){
         this.loading = true;
         axios.get(`${this.store.baseUrl}/api/projects/${this.$route.params.id}`).then((response) => {
-            this.project = response.data.project
-            this.loading = false
+            if(response.data.success){
+                this.project = response.data.project
+                this.loading = false
+            }
+            else{
+
+                this.$router.push({ name:'not-found' })
+
+            }
         })
     }
 }
@@ -24,11 +31,16 @@ export default {
 
 
 <template lang="">
-    <div>
+    <div v-if="loading">
+            <div class="loader">
+            </div> 
+    </div>
+    <div v-else>
         <img :src="project.cover_image != null ? `${this.store.baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/200/300'" alt="">
-        <h1>{{ project.title }}</h1>
-        <p>{{ project.description }}</p>
-        <p v-if="project.type">{{ project.type.name }}</p>
+        <h1>TITOLO: {{ project.title }}</h1>
+        <p>DESCRIZIONE: {{ project.description }}</p>
+        <p v-if="project.type">TIPOLOGIA: {{ project.type.name }}</p>
+        TECNOLOGIE UTILIZZATE:
         <ul v-if="project.technologies">
             <li v-for="technology, index in project.technologies" :key="index">
                 {{ technology.name }}
@@ -39,6 +51,18 @@ export default {
 </template>
 
 
-<style lang="">
-    
+<style lang="scss" scoped>
+        .loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
